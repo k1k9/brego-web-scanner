6b189ad7ecb8b84333435610c79a3557de82411f
Krystian <k1k9@protonmail.com>
2023-05-18 10:24:58 +0200

    Added server scan options

---
+++ src/server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import logging
+import requests as r
+
+from src.functions import test_connection, pick_user_agent
+
+
+class Server:
+    def __init__(self, url):
+        self.URL = url
+        self.logger = logging.getLogger("BregoMain")
+        if not test_connection(self.URL): exit(1)
+        self.logger.debug("Server init")
+        self._loop()
+
+    def _loop(self) -> bool:
+        self._check_server_informations()
+        return True
+    
+    def _check_server_informations(self):
+        rsp = r.options(self.URL, headers={
+            "User-Agent": pick_user_agent()})
+        try: self.logger.info(f"Server: {rsp.headers.get('Server')}")
+        except: pass
+        try: self.logger.info(f"Version: {rsp.headers.get('X-Powered-By')}")
+        except: pass
+
+        try:
+            rsp = r.options(self.URL, headers={"User-Agent": pick_user_agent()})
+            if rsp.headers.get('Allow'):
+                mth = rsp.headers.get('Allow')
+            else: mth = rsp.headers.get('Access-Control-Allow-Methods')
+            self.logger.info(f"Avaliable methods: {mth}")
+        except: pass
+
+
+        
